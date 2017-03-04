5ca6010a8eef89bde9d3465e38b683a978f67de8
Tyler Lucas <tyblu@live.com>
2017-03-03 19:17:28 -0700

    Started HelloWorld. Not working. "No main class selected" or something.

---
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+/*
+            HelloWorld, COMP268
+Class:      HelloWorld.java
+Purpose:    Test IDE and a Java project.
+
+@author:    Tyler Lucas
+Student ID: 3305203
+Date:       March 3, 2017
+Version     1.0
+
+Based on:   Eck, David J. Introduction to programming using Java: Version 7,
+            August 2014. Geneva, NY.: Lulu Press, Inc., 2014. Print.
+*/
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// A program to display the message "Hello World!" on standard output.
+public class HelloWorld {
+
+    private static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}   // end of class HelloWorld
+
+//23456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+//      10        20        30        40        50        60        70        80        90       100
