012d4b35094ef1b640b4f979ef918efb3b2a3fa3
Tyler Lucas <tyblu@live.com>
2017-03-03 17:51:40 -0700

    Pasted previously created java code for Assignment 0, question 1, into repo.

---
+++ Assignment 0/CelsiusToFahrenheit.java
@@ -0,0 +1,57 @@
+/**
+ *             Assignment0, COMP268
+ * Class:      CelciusToFahrenheit.java
+ * Purpose:    Convert degrees Celsius to degrees Fahrenheit.
+ *
+ * @author:    Tyler Lucas
+ * Student ID: 3305203
+ * Date:       October 20, 2016
+ * Version     1.0
+ * 
+ * Based on:   N/A
+ */
+
+/**
+ * Create a new instance of class CelsiusToFahrenheit. Initialize instance
+ * degC, degF variables to values of the arguments passed through the
+ * constructor parameters.
+ * parameter degC    - degrees Celsius
+ * parameter degF    - degrees Fahrenheit
+ */
+public class CelsiusToFahrenheit {
+    // get degC "Enter the temperature in Celsius: "
+    // error handling
+    // init degF
+    // calculate degF  F=C*(9/5)+32
+    // output degF "The temperature in Fahrenheit is "
+    // return??
+ }      
+
+
+
+
+/**                               THIS LINE IS 100 CHARACTERS WIDE                               **/
+
+
+
+
+
+
+
+
+//package celciustofahrenheit;
+//
+///**
+// *
+// * @author Tyler
+// */
+//public class CelciusToFahrenheit {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
+//    
+//}
