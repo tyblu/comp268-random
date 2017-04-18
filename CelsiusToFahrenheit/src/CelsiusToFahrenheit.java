9b619a8001ea9cef7e1c22bb580d292713690994
Tyler Lucas <tyblu@live.com>
2017-04-18 10:14:14 -0600

    Got rid of fluff code. Program contains only code required for basic functionality. Won't be making it more complex until I have a better grasp on passing parameters between functions, and even then, only if warranted.

--- CelsiusToFahrenheit/src/CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit/src/CelsiusToFahrenheit.java
@@ -5,16 +5,17 @@
  *
  * @author:    Tyler Lucas
  * Student ID: 3305203
- * Date:       April 17, 2017
- * Version     1.2
+ * Date:       April 18, 2017
+ * Version     1.3
  */
 
 /**
  * Create a new instance of class CelsiusToFahrenheit. Initialize instance
  * degC, degF variables to values of the arguments passed through the
  * constructor parameters.
- * parameter degC    - degrees Celsius
- * parameter degF    - degrees Fahrenheit
+ *
+ * parameter degC    - input; degrees Celsius
+ * parameter degF    - output; degrees Fahrenheit
  */
 
 import java.util.Scanner;
@@ -27,22 +28,10 @@ public class CelsiusToFahrenheit {
         
         int degC, degF;
         
-//        startText();
-        
         // get degC "Enter the temperature in Celsius: "
         System.out.print("Enter temperature in integer degrees Celsius: ");
         degC = sc.nextInt();
         sc.nextLine();
-//        degC = getIntFromInput();
-        
-        // error handling
-//        if ( degC < -273 ) {
-//            System.out.println("\nError. Temperature too low. Adjusting.");
-//            degC = -273;
-//        }
-        
-//        System.out.printf("%nThe temperature has been set to %d \u00b0C.",degC);
-//        System.out.println();
         
         // calculate degF  F=C*(9/5)+32
         degF = degC * 9 / 5 + 32;
@@ -50,40 +39,6 @@ public class CelsiusToFahrenheit {
         // output degF "The temperature in Fahrenheit is "
         System.out.printf("%n%d degrees Celsius is equivalent to %d degrees "
                 + "Fahrenheit.", degC, degF);
-    }
-    
-//    private static void print3Dots() {
-//        int i;
-//        
-//        for ( i=0; i<3; i++ ) {
-//            Thread.sleep(250);
-//            System.out.print(".");
-//        }
-//    }
-    
-//    private static void startText() {
-//        System.out.print("\nStarting temperature conversionator");
-//        print3Dots();
-//        System.out.println();
-//    }
-    
-//    private static int getIntFromInput() {
-//        int inputInt, k = 0;
-//
-//        while( !sc.hasNextInt() ) { // catch and skip non-numerical input chars
-//            if ( k++ < 1 ) { // give user error feedback
-//                System.out.println("Error. Skipping non-integer input. ");
-//            }
-//            sc.next();
-//        }
-//        
-//        inputInt = sc.nextInt();
-//        
-//        return inputInt;
-//    }
- }      
-
-
-
-
-/**                               THIS LINE IS 100 CHARACTERS WIDE                               **/
+        
+    } //end main
+ }
