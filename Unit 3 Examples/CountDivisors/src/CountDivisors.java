8801ceb77a61810380fb2f0be8f62dffb6f81c0a
Tyler Lucas <tyblu@live.com>
2017-05-11 15:39:05 -0600

    Upgraded example to version 1.1, with my own tweaks.

--- Unit 3 Examples/CountDivisors/src/CountDivisors.java
+++ Unit 3 Examples/CountDivisors/src/CountDivisors.java
@@ -3,11 +3,12 @@
  * Class:      CountDivisors.java
  * Purpose:    This program reads a positive integer from the user. It counts
  *             how many divisors that number has, and then it prints the result.
+ *             Now also prints divisors (including 1 and itself).
  * 
  * @author:    Tyler Lucas
  * Student ID: 3305203
  * Date:       May 11, 2017
- * Version     1.0 (Nearly identical to textbook implementation.)
+ * Version     1.1 (Nearly identical to textbook implementation.)
  * 
  * Based on:   Eck, pp 92-93
  * 
@@ -19,32 +20,41 @@ public class CountDivisors {
 
     public static void main(String[] args) {
         
-        int number;
-        int testDivisor;
-        int divisorCount;
-        int divisorsTested; // Only counts numbers below 1e6 (i.e. total%1e6).
+        int number = 0;
+        int testDivisor = 1;
+        int divisorCount = 1;
+        int divisorsTested = 1; // Only counts numbers below 1e6 (i.e. total%1e6).
+        String strDivisors;     // CSV of divisors.
         
         /* Get a positive integer from the user. */
         
-        while ( true ) {
-            System.out.print( "Enter a positive integer: ");
-            number = TextIO.getlnInt();
-            
-            if ( number > 0 )    // This 
-                break;
-            
+        System.out.print( "Enter a positive integer: ");
+        number = TextIO.getlnInt();
+        
+        while ( number < 1 ) {
             System.out.println( "That number is not positive."
                     + " Please try again." );
+            number = TextIO.getlnInt();
         }
         
+//        do {
+//            number = TextIO.getlnInt();
+//            if ( number < 1 ) {
+//                System.out.println( "That number is not positive."
+//                        + " Please try again." );
+//            }
+//        } while ( number < 1 );
+
         /* Count the divisors, printing a "." after every 1000000 tests. */
         
-        divisorCount = 0;
-        divisorsTested = 0;
+        strDivisors = Integer.toString( 1 ) + ", ";
         
-        for ( testDivisor = 1; testDivisor <=number; testDivisor++ ) {
-            if ( number % testDivisor == 0 )
+        for ( testDivisor = 2; testDivisor <=number; testDivisor++ ) {
+            if ( number % testDivisor == 0 ) {
                 divisorCount++;
+                strDivisors += Integer.toString( testDivisor ) + ", ";
+            }
+            
             
             divisorsTested++;
             if ( divisorsTested == 1000000 ) {
@@ -57,9 +67,12 @@ public class CountDivisors {
         
         System.out.println();
         System.out.printf( 
-                "The number of divisors of %d is %d.%n", 
+                "The number of divisors of %d (including 1 and itself) is %d, as follows:%n", 
                 number, 
                 divisorCount );
+        // Strip last comma and space.
+        strDivisors = strDivisors.substring( 0,strDivisors.length()-2 );
+        System.out.println( strDivisors );
     }
     
 }
