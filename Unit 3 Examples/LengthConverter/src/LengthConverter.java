709cae2eb303112b776b6524de9d5fb6472e30cd
Tyler Lucas <tyblu@live.com>
2017-05-12 14:46:45 -0600

    Textbook example from unit 3: LengthConverter, initial commit.

---
+++ Unit 3 Examples/LengthConverter/src/LengthConverter.java
@@ -0,0 +1,30 @@
+/**
+ *             Textbook Example Program
+ * Class:      Length Converter.java
+ * Purpose:    This program will convert measurements expressed in inches,
+ *             feet, yards, or miles into each of the possible units of measure.
+ *             The measurement is input by the user, followed by the unit of
+ *             measure. The measurement is input by the user, followed by the
+ *             unit of measure. For example: "17 feet", "1 inch", or "2.73 mi".
+ *             Abbreviations "in", "ft", "yd", and "mi" are accepted. The
+ *             program will continue to read and convert measurements until the
+ *             user enters an input of 0.
+ * 
+ * @author:    Tyler Lucas
+ * Student ID: 3305203
+ * Date:       May 12, 2017
+ * Version     1.0 (Nearly identical to textbook implementation.)
+ * 
+ * Based on:   Eck, pp 101-103
+ * 
+ * References: 
+ * 
+ */
+
+public class LengthConverter {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
