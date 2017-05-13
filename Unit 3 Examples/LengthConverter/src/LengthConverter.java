6f4f098b7c00d4f3d08c72c97d99e5b7aedd3e33
Tyler Lucas <tyblu@live.com>
2017-05-12 19:18:53 -0600

    Added two more ways to print out that huge bit of introductory text, both reading from a text file. The first ("Method #2") uses Scanner and the second ("Method #3") uses Stream<String>. Also added framework to swap between these options, like you can with #ifdefs etc and the C preprocessor, using private static final boolean variables. It's clean enough.

--- Unit 3 Examples/LengthConverter/src/LengthConverter.java
+++ Unit 3 Examples/LengthConverter/src/LengthConverter.java
@@ -13,15 +13,35 @@
  * @author:    Tyler Lucas
  * Student ID: 3305203
  * Date:       May 12, 2017
- * Version     1.0 (Nearly identical to textbook implementation.)
+ * Version     1.1
  * 
  * Based on:   Eck, pp 101-103
  * 
- * References: 
+ * References: https://stackoverflow.com/questions/1813853/ifdef-ifndef-in-java
  * 
  */
 
+// For I/O method #2.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+// For I/O method #3.
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
 public class LengthConverter {
+    
+    private static final boolean ENABLEMETHOD1 = true;
+    private static final boolean ENABLEMETHOD2 = false;
+    private static final boolean ENABLEMETHOD3 = false;
 
     public static void main(String[] args) {
         
@@ -30,22 +50,45 @@ public class LengthConverter {
         
         double inches, feet, yards, miles, meters, kilometers;
         
-        System.out.print(
-                "Enter measurements in inches, feet, yards, miles, meters, or kilometers.\n"
-                + "You can use abbreviations: in, ft, yd, mi, km. Plurality is also allowed.\n"
-                + "For example:\t1 inch\n"
-                + "\t\t48 inches\n"
-                + "\t\t6 in\n"
-                + "\t\t17 feet\n"
-                + "\t\t23 ft\n"
-                + "\t\t0.4 miles\n"
-                + "\t\t2.73 mi\n"
-                + "\t\t3.1 kilometers\n"
-                + "\t\t1 kilometre\n"
-                + "\t\t2 km\n"
-                + "I will convert your input into the other units of measure.\n"
-        );
-        
+        // Method #1 to print the intro text.
+        if ( ENABLEMETHOD1 ) {
+            
+            System.out.print(
+                    "Enter measurements in inches, feet, yards, miles, meters, or kilometers.\n"
+                    + "You can use abbreviations: in, ft, yd, mi, km. Plurality is also allowed.\n"
+                    + "For example:\t1 inch\n"
+                    + "\t\t48 inches\n"
+                    + "\t\t6 in\n"
+                    + "\t\t17 feet\n"
+                    + "\t\t23 ft\n"
+                    + "\t\t0.4 miles\n"
+                    + "\t\t2.73 mi\n"
+                    + "\t\t3.1 kilometers\n"
+                    + "\t\t1 kilometre\n"
+                    + "\t\t2 km\n"
+                    + "I will convert your input into the other units of measure.\n"
+            );
+        }
+
+        // Method #2 to print the intro text.
+        if ( ENABLEMETHOD2 ) {
+            try {
+                Scanner scan = new Scanner(new File( "C:\\Users\\tyblu\\Documents\\repos\\comp268-random\\Unit 3 Examples\\LengthConverter\\src\\intro.txt"));
+                while( scan.hasNextLine() ) { System.out.println( scan.nextLine() ); }
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(LengthConverter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        // Method #3 to print the intro text.
+        if ( ENABLEMETHOD3 ) {
+            try ( Stream<String> lines = Files.lines(Paths.get("C:\\Users\\tyblu\\Documents\\repos\\comp268-random\\Unit 3 Examples\\LengthConverter\\src\\intro.txt"), Charset.defaultCharset() ) ) {
+                lines.forEachOrdered(System.out::println);
+            } catch (IOException ex) {
+                Logger.getLogger(LengthConverter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
         while (true) {
             
             /* Get the user's input, and convert units to lower case. */
