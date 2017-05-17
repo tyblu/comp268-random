bd66657d9168b5c617a11b2fafc8ab384674064a
Tyler Lucas <tyblu@live.com>
2017-05-17 14:05:40 -0600

    Debugging finished. Program works as intended. Documentation complete. Moving on.

--- Unit 3 Examples/ParseSalesData/src/ParseSalesData.java
+++ Unit 3 Examples/ParseSalesData/src/ParseSalesData.java
@@ -1,23 +1,20 @@
-
-import java.text.ParseException;
-
 /**
  *              Textbook Exercise Program
  * Class:       ParseSalesData.java
  * Purpose:     Parses sales data from file sales.dat. Outputs total sales and
  *              number of cities for which data was not available.
  * 
+ *      v1.0    Works! Debugging output removed.
  *      v0.1    Outputs a bunch of values and text to tell you what's going on.
  * 
  * @author:    Tyler Lucas
  * Student ID: 3305203
  * Date:       May 17, 2017
- * Version     0.1 Alpha version.
+ * Version     1.0
  * 
  * Based on:    AverageNumbersFromFile (Eck, p113)
  * 
  * References:  Eck p130, Unit 3 Exercise 5
- *              https://stackoverflow.com/questions/3681242/java-how-to-parse-double-from-regex
  * 
  */
 
@@ -41,20 +38,11 @@ public class ParseSalesData {
             // Read line
             String strLine = TextIO.getln();
             
-            System.out.println();
-            System.out.printf("Sum before:   %f%nSkips before: %d%n",sum,skippedCount);
-            System.out.print("Line: ");
-            System.out.println( strLine );
-            
             // Parse line
             try {   // .substring(1) is to allow 1st .substring(':') to throw exception
                 strLine = strLine.substring(strLine.indexOf(':')).substring(1).trim();
             } catch (IndexOutOfBoundsException e) {
                 // No ':'. Skip to next line
-                System.out.print("No : in \"");
-                System.out.print(strLine);
-                System.out.println("\"");
-                System.out.println("Skipping to next line.");
                 continue;
             }
             
@@ -62,19 +50,13 @@ public class ParseSalesData {
                 strLine = strLine.substring(strLine.indexOf('$')).substring(1).trim();
             } catch (IndexOutOfBoundsException e) {
                 // No '$'. Do nothing.
-                System.out.print("No $ in \"");
-                System.out.print(strLine);
-                System.out.println("\"");
-                System.out.println("Going on with my life.");
             }
             
             // Add to sum or add to skipped tally
             try {
                 sum += Double.parseDouble(strLine);
-                System.out.println("Added "+Double.parseDouble(strLine)+" to sum.");
             } catch (NumberFormatException e) {
                 if ( strLine.length() > 2 ) { skippedCount++; }
-                System.out.println("Added +1 to skippedCount ("+skippedCount+")");
             }
         }
             
@@ -85,5 +67,4 @@ public class ParseSalesData {
                 sum, skippedCount
         );
     }
-    
 }
