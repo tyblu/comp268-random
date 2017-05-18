ea24301d1df69ab9090f662094880a1d4f4922d8
Tyler Lucas <tyblu@live.com>
2017-05-18 17:04:26 -0600

    This won't do anything until I run it from the command line and give it arguments.

---
+++ Unit 4 Examples/CommandLineDemo/src/CommandLineDemo.java
@@ -0,0 +1,29 @@
+/**
+ *              Textbook Example Program
+ * Class:       CommandLineDemo.java
+ * Purpose:     Prints command line arguments.
+ * 
+ * @author:    Tyler Lucas
+ * Student ID: 3305203
+ * Date:       May 18, 2017
+ * Version     1.1 (Enhanced for loop.)
+ * 
+ * Based on:   Eck pp 152
+ * 
+ * References:
+ * 
+ */
+
+public class CommandLineDemo {
+
+    public static void main(String[] args) {
+        System.out.println("You entered " + args.length + " command-line arguments.");
+        
+        if ( args.length > 0 ) {
+            System.out.println("There were:");
+            for (String arg : args) {
+                System.out.println("\t" + arg);
+            }
+        }
+    }
+}
