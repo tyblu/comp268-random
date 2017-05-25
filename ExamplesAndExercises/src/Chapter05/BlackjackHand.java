a22dd5d5aa54d3d655f89b608aaba69d3fe343ca
Tyler Lucas <tyblu@live.com>
2017-05-25 17:36:07 -0600

    BlackjackHand completed. Nearly identical to textbook implementation. No test for it (yet?), but NetBeans says it's a-okay.

--- ExamplesAndExercises/src/Chapter05/BlackjackHand.java
+++ ExamplesAndExercises/src/Chapter05/BlackjackHand.java
@@ -24,13 +24,24 @@
 package Chapter05;
 
 /**
- *
- * @author Tyler Lucas <tyblu@live.com>
+ *              Textbook Example Program, Chapter 5
+ * Class:       BlackjackHand.java
+ * Purpose:     
+ * 
+ * @author:    Tyler Lucas
+ * Student ID: 3305203
+ * Date:       May 25, 2017
+ * Version     1.0
+ * 
+ * Based on and References:
+ * @see Introduction to Programming Using Java Version 7, by Eck, David J., 
+ *      2014: Chapter 5
+ * 
  */
 public class BlackjackHand extends Hand 
 {
     /**
-     * @returns The Computed value of this hand in the game of Blackjack.
+     * @return The Computed value of this hand in the game of Blackjack.
      */
     public int getBlackjackValue()
     {
@@ -42,12 +53,24 @@ public class BlackjackHand extends Hand
         hasAce = false;
         cardCount = getCardCount(); // method in class Hand
         
-        for (int i=0; i<cards; i++)
+        for (int i=0; i<cardCount; i++)
         {
             // Add value of i-th card in the hand.
             Card card;      // the i-th card
             int cardVal;    // Blackjack value of i-th card
+            
+            card = getCard(i);
+            cardVal = card.getValue();  // non-Blackjack value
+            if (cardVal > 10) { cardVal = 10; } // Jack, Queen, King are 10 pts
+            if (cardVal == 1) { hasAce = true; }
+            
+            valHand += cardVal;
         }
+        
+        // If ace value was 11 (+10), would it be better? If so, do it.
+        if ( hasAce && valHand + 10 <= 21 )
+            valHand += 10;
+        
+        return valHand;
     }
-    
 }
