3190c410b8d7e8be5443e9607b3669ea010835fb
Tyler Lucas <tyblu@live.com>
2017-06-19 11:30:33 -0600

    Works. Going to center dice in display, now.

--- Unit 6 Examples/src/Chapter06/Exercise3.java
+++ Unit 6 Examples/src/Chapter06/Exercise3.java
@@ -25,17 +25,13 @@ package Chapter06;
  */
 
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.geom.RoundRectangle2D;
-import java.awt.image.BufferedImage;
 import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -60,7 +56,7 @@ import javax.swing.JPanel;
  * @author:     Tyler Lucas
  * Student ID:  3305203
  * Date:        June 15, 2017
- * Version      0.1
+ * Version      0.2
  * 
  * Based on and References:
  * @see <a href="http://math.hws.edu/javanotes/">
@@ -116,13 +112,13 @@ public class Exercise3
     private class RollingDicePanel extends JPanel
     {
         // Instance variables.
-        private DiceAnimated dice1, dice2;
+        private Dice dice1, dice2;
         
         // Constructor.
         public RollingDicePanel()
         {
-            this.dice1 = new DiceAnimated();
-            this.dice2 = new DiceAnimated();
+            this.dice1 = new Dice();
+            this.dice2 = new Dice();
             
             setSize(0.5, 0.5);
 
@@ -132,7 +128,7 @@ public class Exercise3
             add(dice1);
             add(dice2);
             
-
+            
             addMouseListener( new MouseAdapter() {
                     @Override
                     public void mousePressed(MouseEvent evt) { roll(); }
@@ -146,7 +142,8 @@ public class Exercise3
          */
         private void roll()
         {
-            // 
+            System.out.println("DICE 1: " + dice1.roll());
+            System.out.println("DICE 2: " + dice2.roll());
         }
         
         /**
@@ -159,82 +156,135 @@ public class Exercise3
                     (int)(screenDim.width * xRatio),
                     (int)(screenDim.height * yRatio) ));
         }
-        
-        @Override
-        public void paintComponent(Graphics g)
-        {
-            super.paintComponent(g);
-            
-            drawDice(g, new DiceAnimated[]{ dice1, dice2 });
-        }
-        
-        private void drawDice(Graphics g, DiceAnimated[] diceArray)
-        {
-            // split up panel -- leave to LayoutManager?
-            
-            // draw dice
-            for (DiceAnimated dice : diceArray)
-            {
-                
-            }
-        }
-        
-        private void drawRect(CustomRectangle r, Graphics g)
-        {
-            try { new Debug(this, new Object[]{ r }, DEBUG_ON); }
-            catch (NullPointerException e) { System.out.println(e); }
-            
-            g.setColor(r.getFillColor());
-            g.fillRect(
-                    r.getLocation().x, 
-                    r.getLocation().y, 
-                    (int)r.getWidth(), 
-                    (int)r.getHeight()
-            );
-            g.setColor(r.getOutlineColor());
-            g.drawRect(
-                    r.getLocation().x, 
-                    r.getLocation().y, 
-                    (int)r.getWidth(), 
-                    (int)r.getHeight()
-            );
-        }
     }
     
-    private class DiceAnimated extends Dice
+    private class Dice extends JPanel
     {
         // Instance variables.
-        private DiceImage[] faces;
-        private int sequenceIndex;
-        private DiceFace currentFace;
-        
-    }
-    
-    private class Dice
-    {
-        // Instance variables.
-        private int sides;
         private int value;
         private int rollCount;
         private Random r;
         
         // Constructors.
-        public Dice(int sides)
+        public Dice()
         {
-            this.sides = sides;
             this.r = new Random();
-            this.value = r.nextInt(this.sides) + 1;
+            this.value = r.nextInt(6) + 1;
             this.rollCount = 1;
         }
-        
-        public Dice() { this(6); }
         
         // Methods.
         public int roll()
         {
-            setValue(r.nextInt(sides) + 1);
+            setValue(r.nextInt(6) + 1);
             rollCount++;
+            
+            repaint();
+            
             return getValue();
+        }
+        
+        @Override
+        protected void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+            
+            g.setColor(Color.BLACK);
+            g.fill3DRect(0, 0, 256, 256, true);
+            g.setColor(Color.WHITE);
+            g.fill3DRect(16, 16, 256-2*16, 256-2*16, false);
+            drawSpots(g, value);
+        }
+        
+        private void drawSpots(Graphics g, int value)
+        {
+            switch (value)
+            {
+            case 1:
+                drawSpot(g, 4);
+                break;
+            case 2:
+                drawSpot(g, 0);
+                drawSpot(g, 8);
+                break;
+            case 3:
+                drawSpots(g, 2);
+                drawSpot(g, 4);
+                break;
+            case 4:
+                drawSpots(g, 2);
+                drawSpot(g, 2);
+                drawSpot(g, 6);
+                break;
+            case 5:
+                drawSpots(g, 4);
+                drawSpot(g, 4);
+                break;
+            case 6:
+                drawSpots(g, 4);
+                drawSpot(g, 3);
+                drawSpot(g, 5);
+                break;
+            default:    // All spots. Never happens for actual dice values 1-6.
+                drawSpots(g, 6);
+                drawSpot(g, 1);
+                drawSpot(g, 4);
+                drawSpot(g, 7);
+                break;
+            }
+        }
+        
+        /**
+         *  _______
+         * | 0 1 2 |
+         * | 3 4 5 |
+         * |_6_7_8_|
+         * 
+         * dice width 256, border 8, edge gap 16, spot diameter 32, gap computed
+         */
+        private void drawSpot(Graphics g, int spotNumber)
+        {
+            int gap = (256 - 2*8 - 2*16 - 3*32)/2;  // = 56
+            int[] col = new int[] {
+                        256/2 - gap - 32/2,
+                        256/2 - 32/2,
+                        256/2 + gap - 32/2
+                    };
+            int[] row = col;
+            
+            g.setColor(Color.BLACK);
+            switch (spotNumber)
+            {
+            case 0:
+                g.fillOval(col[0], row[0], 32, 32);
+                break;
+            case 1:
+                g.fillOval(col[1], row[0], 32, 32);
+                break;
+            case 2:
+                g.fillOval(col[2], row[0], 32, 32);
+                break;
+            case 3:
+                g.fillOval(col[0], row[1], 32, 32);
+                break;
+            case 4:
+                g.fillOval(col[1], row[1], 32, 32);
+                break;
+            case 5:
+                g.fillOval(col[2], row[1], 32, 32);
+                break;
+            case 6:
+                g.fillOval(col[0], row[2], 32, 32);
+                break;
+            case 7:
+                g.fillOval(col[1], row[2], 32, 32);
+                break;
+            case 8:
+                g.fillOval(col[2], row[2], 32, 32);
+                break;
+            default:
+                break;
+            }
         }
         
         public void resetRollCount() { setRollCount(0); };
@@ -248,101 +298,6 @@ public class Exercise3
         // Setters.
         private void setValue(int value) { this.value = value; }
         private void setRollCount(int rollCount) { this.rollCount = rollCount; }
-    }
-    
-    private class DiceFace extends JPanel
-    {
-        
-    }
-    
-    private class DiceFaces extends RoundRectangle2D.Double
-    {
-        // Instance variables.
-        private final int faceSize;
-        private final int spotSize;
-        private final int border;
-        private final Point[] spotSpots;
-        private Image[] faces;
-        
-        public DiceFaces()
-        {
-            this.faceSize = 256;
-            this.spotSize = faceSize / 8;
-            this.border = faceSize / 32;
-            this.spotSpots = getSpotSpots();
-            
-            for (int i=0; i<6; i++)
-                faces[i] = new BufferedImage(faceSize, faceSize,
-                        BufferedImage.TYPE_INT_RGB);
-        }
-        
-        // Methods.
-        public Image drawFaces(int count)
-        {
-            Graphics g = faces[count-1].getGraphics();
-            
-            drawBorder(g);
-            drawBackground(g);
-            drawSpots(g);
-            
-            g.dispose();
-            return faces[count-1];
-        }
-        
-        private void drawBorder(Graphics g)
-        {
-            g.setColor(Color.BLACK);
-            g.fillRect(0, 0, w, h);
-        }
-        
-        private void drawBackground(Graphics g)
-        {
-            g.setColor(Color.WHITE);
-            g.fillRect(0, 0, w - border, h - border);
-        }
-        
-        private void drawSpots(Graphics g)
-        {
-            switch (count)
-            {
-            case 1:
-                drawSpot(g, "3");
-                break;
-            case 2:
-                drawSpot
-            }
-        }
-        
-        // Getters.
-        /**
-         * Computes spot locations based on face and spot sizes.     _________
-         * @return array of Point[] for spot locations in order      | 0 1 2 |
-         *      shown in the diagram (0-6).                          |   3   |
-         *                                                           |_4_5_6_|
-         */
-        private Point[] getSpotSpots()
-        {
-            Point[] locs = new Point[6];
-            
-            // Point 3 is in the very center
-            locs[3] = new Point(faceSize / 2, faceSize / 2);
-            
-            // Compute ideal gap between spots and edge
-            int gap = (int)((faceSize - 2* border - 2* spotSize) / (double)4);
-            
-            // Points 1 and 5
-            locs[1] = new Point(locs[3].x, locs[3].y + gap + spotSize / 2);
-            locs[5] = new Point(locs[3].x, locs[3].y - gap - spotSize / 2);
-            
-            // Points 0, 2, 4, and 6
-            locs[0] = new Point(locs[1].x - gap - spotSize / 2, locs[1].y);
-            locs[2] = new Point(locs[1].x + gap + spotSize / 2, locs[1].y);
-            locs[4] = new Point(locs[5].x - gap - spotSize / 2, locs[5].y);
-            locs[6] = new Point(locs[5].x + gap + spotSize / 2, locs[5].y);
-            
-            return locs;
-        }
-        
     }
     
     // Methods
