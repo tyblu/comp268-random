e484fd6005c68268b7de222ed89e998a02e6dfd1
Tyler Lucas <tyblu@live.com>
2017-06-24 02:30:31 -0600

    Initial commit of exercise #4, chapter 7.

---
+++ Unit 7 Exercise 4/src/Chapter07E04/BetterStamper.java
@@ -0,0 +1,51 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017 Tyler Lucas <tyblu@live.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package Chapter07E04;
+
+import java.util.ArrayList;
+
+/**
+ * <h1>Eck v7, Chapter 7, Exercise #4</h1>
+ * 
+ * Revise the program SimpleStamperWithDrag from Exercise #1 in chapter 6 such
+ * that the panel stores paint stroke information in an ArrayList and redraws
+ * them correctly.
+ *
+ * @author:     Tyler Lucas
+ * Student ID:  3305203
+ * Date:        June 24, 2017
+ * Version      1.0
+ * 
+ * Based on and References:
+ * @see <a href="http://math.hws.edu/javanotes/">
+ *      <cite>Introduction to Programming Using Java, Seventh Edition</cite>,
+ *      by Eck, David J., 2014: Chapter 7: Arrays and ArrayLists</a>
+ */
+public class BetterStamper
+{
+    public static void main(String[] args)
+    {
+        // test
+    }
+}
