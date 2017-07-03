36d2ebd13b62dfb23e4f6ceb904a147c52a4afd5
Tyler Lucas <tyblu@live.com>
2017-07-03 14:00:25 -0600

    Renamed package (and refactored). Was having issues with lowercase package name showing up, and could not change it to ProperCase and have git recognize the change (I don't think git sees changes to directory as significant, only files).

--- NumberPrecision/src/NumberPrecisionDemo/NumberPrecision.java
+++ NumberPrecision/src/NumberPrecisionDemo/NumberPrecision.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package NumberPrecision;
+package NumberPrecisionDemo;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
